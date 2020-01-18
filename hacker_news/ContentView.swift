9ca2540cbb9923d3bb121653b9212b93635c7007
MarkStudio <markchen8024@gmail.com>
2020-01-18 22:58:28 +0800

    Initial Commit

---
+++ hacker_news/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hacker_news
+//
+//  Created by Mark Chen on 1/18/20.
+//  Copyright © 2020 Mark Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
