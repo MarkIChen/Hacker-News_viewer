0a898820031cb8835b4f6c765698129fef242a5e
MarkStudio <markchen8024@gmail.com>
2020-01-23 10:10:34 +0800

    add List and URLManager components

---
+++ hacker_news/Module/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  hacker_news
+//
+//  Created by Mark Chen on 1/20/20.
+//  Copyright © 2020 Mark Chen. All rights reserved.
+//
+
+import Foundation
---
+++ hacker_news/Module/PostData.swift
@@ -0,0 +1,9 @@
+//
+//  PostData.swift
+//  hacker_news
+//
+//  Created by Mark Chen on 1/21/20.
+//  Copyright © 2020 Mark Chen. All rights reserved.
+//
+
+import Foundation
